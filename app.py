1cb3219b4a5d56553c77d45727e8edc0ef698277
sidoriginal <strivedi_be20@thapar.edu>
2022-10-29 19:05:10 +0530

    -

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ def index():
     return "Hello world"
 @app.route('/predict',methods=['POST'])
 def predict():
-    Ma= request.form.get('Ma')
-    Fe= request.form.get('Fe')
+    gender= request.form.get('gender')
     age = request.form.get('age')
     smoke = request.form.get('smoke')
     yellow = request.form.get('yellow')
@@ -24,6 +23,14 @@ def predict():
     breath = request.form.get('breath')
     swallow = request.form.get('swallow')
     chest = request.form.get('chest')
+    Ma=""
+    Fe=""
+    if(gender=='M'):
+        Ma="1"
+        Fe="0"
+    else:
+        Fe="1"
+        Ma="1"
     input_query = np.array([[Fe,Ma,age,smoke,yellow,anxiety,peer,chronic,fatigue,allergy,wheezing,alcohol,cough,breath,swallow,chest]])
     result = model.predict(input_query)[0]
     return jsonify({'cancer':str(result)})
